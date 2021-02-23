39ef01c3d64e6423d48d0ea480a19b712195a54f
Pavel-Liteiniy <paulsev96@gmail.com>
2021-02-23 17:50:43 +0300

    Добавляет возможность удалять задачи из списка

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,26 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
 
-import AppHeader from '../app-header';
-import SearchPanel from '../search-panel';
-import TodoList from '../todo-list';
-import ItemStatusFilter from '../item-status-filter';
+import AppHeader from '../app-header'
+import SearchPanel from '../search-panel'
+import TodoList from '../todo-list'
+import ItemStatusFilter from '../item-status-filter'
 
-import './app.css';
+import './app.css'
+
+const EventType = {
+  DELETE: `delete`,
+}
+
+const deleteItem = (items, index) => {
+  if (index === -1) {
+    return items
+  }
+
+  return [
+    ...items.slice(0, index),
+    ...items.slice(index + 1)
+  ]
+}
 
 export default class App extends Component {
   state = {
@@ -23,8 +38,24 @@ export default class App extends Component {
   
         <TodoList 
           tasks={ this.state.tasks }
-          appEventHandler={ (type, id) => console.log(type, id) } />
+          appEventHandler={ (type, data) => this._updateTasks(type, data) } />
       </div>
     )
+  }
+  
+  _updateTasks = (type, data) => {
+    switch (type) {
+      case EventType.DELETE:
+        this.setState(({tasks}) => {
+          const index = tasks.findIndex((task) => {
+            return task.id === data
+          })
+
+          const updatedTasks = deleteItem(tasks, index)
+
+          return {tasks: updatedTasks}
+        })
+        break
+    }
   }
 }
